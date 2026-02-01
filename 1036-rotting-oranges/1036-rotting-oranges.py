04cf4caaa01210179137813b76da41a2970ca143
Shvet Mehra <shvetmehra@gmail.com>
2026-02-01 17:22:58 +0530

    1036-ROTTING-ORANGES Stats: Time: 6 ms (55.78%), Space: 12.4 MB (89.6%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,4 +1,5 @@
 from collections import deque
+from copy import deepcopy
 class Solution(object):
     def orangesRotting(self, grid):
         """
@@ -6,36 +7,35 @@ class Solution(object):
         :rtype: int
         """
         rows = len(grid)
-        cols = len(grid[0])
-        grid_copy = deepcopy(grid)
+        cols= len(grid[0])
+        gridCopy = deepcopy(grid)
 
         freshCount = 0
         queue = deque()
 
         for r in range(rows):
             for c in range(cols):
-                if grid_copy[r][c]==2:
+                if gridCopy[r][c]==2:
                     queue.append((r,c))
-                elif grid_copy[r][c]==1:
+                elif gridCopy[r][c]==1:
                     freshCount +=1
+
         minute = 0
         while len(queue)!=0 and freshCount>0:
             minute+=1
-            totalRotten = len(queue)
-
-            for i in range (totalRotten):
+            totalRotten=len(queue)
+            for i in range(totalRotten):
                 i,j = queue.popleft()
-                for dx, dy in [(0,1), (1,0),(-1,0), (0,-1)]:
-                    newi, newj = i+dx, j+dy
+                for dx,dy in [(-1,0),(0,-1),(1,0),(0,1)]:
+                    newi = i+dx
+                    newj = j+dy
                     if newi<0 or newi==rows or newj<0 or newj==cols:
                         continue
-                    if grid_copy[newi][newj] == 0 or grid_copy[newi][newj]==2:
+                    if gridCopy[newi][newj]==0 or gridCopy[newi][newj]==2:
                         continue
-                    freshCount -=1
-                    grid_copy[newi][newj]=2
+                    freshCount-=1
+                    gridCopy[newi][newj]=2
                     queue.append((newi,newj))
         if freshCount>0:
             return -1
         return minute
-
-        
