8df6d1982b39b6f04c38d3fe537a2820f438ac64
Shvet Mehra <shvetmehra@gmail.com>
2026-02-04 19:29:25 +0530

    0200-NUMBER-OF-ISLANDS Stats: Time: 238 ms (42.7%), Space: 29.6 MB (31.17%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,31 @@
 class Solution(object):
+    def dfs(self, r,c, rows, cols, visited, grid):
+        if r < 0 or c<0 or r>=rows or c>=cols:
+            return
+        if grid[r][c]=="0":
+            return
+        if visited[r][c]==1:
+            return
+        visited[r][c]=1
+        self.dfs(r+1,c, rows, cols, visited, grid)
+        self.dfs(r-1,c, rows, cols, visited, grid)
+        self.dfs(r,c+1, rows, cols, visited, grid)
+        self.dfs(r,c-1, rows, cols, visited, grid)
+
     def numIslands(self, grid):
         """
         :type grid: List[List[str]]
         :rtype: int
         """
-        rows,cols=len(grid),len(grid[0])
         count = 0
-
-        def dfs(r,c):
-            if not(0<=r<rows and 0<=c<cols and grid[r][c] =='1'):
-                return
-            grid[r][c]='0'
-
-            dfs(r+1,c)
-            dfs(r-1,c)
-            dfs(r,c+1)
-            dfs(r,c-1)
+        rows = len(grid)
+        cols = len(grid[0])
+        visited = [[0 for _ in range (cols)] for _ in range (rows)]
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c]=='1':
-                    dfs(r,c)
+                if grid[r][c]=="1" and visited[r][c]==0:
                     count +=1
-        return count
+                    self.dfs(r,c, rows, cols, visited, grid)
     
+        return count
