299eb687fbfaf5b2c02cab81bf11e9b164fc67dc
Shvet Mehra <shvetmehra@gmail.com>
2026-02-05 11:34:32 +0530

    0200-NUMBER-OF-ISLANDS Stats: Time: 240 ms (40.48%), Space: 29.3 MB (36.06%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,31 +1,39 @@
+from collections import deque
 class Solution(object):
-    def dfs(self, r,c, rows, cols, visited, grid):
-        if r < 0 or c<0 or r>=rows or c>=cols:
-            return
-        if grid[r][c]=="0":
-            return
-        if visited[r][c]==1:
-            return
-        visited[r][c]=1
-        self.dfs(r+1,c, rows, cols, visited, grid)
-        self.dfs(r-1,c, rows, cols, visited, grid)
-        self.dfs(r,c+1, rows, cols, visited, grid)
-        self.dfs(r,c-1, rows, cols, visited, grid)
+    def bfs (self, i, j, grid, visited):
+        rows = len(grid)
+        cols = len(grid[0])
+        count = 0
+        queue = deque()
+        queue.append((i,j))
+        visited[i][j]=1
+
+        while len(queue)!=0:
+            x,y = queue.popleft()
+            for xx, yy in [(0,1),(0,-1),(1,0),(-1,0)]:
+                newi, newj = x+xx, y+yy
+                if newi<0 or newj<0 or newi>=rows or newj>=cols:
+                    continue
+                if grid[newi][newj]=='0':
+                    continue
+                if visited[newi][newj]==1:
+                    continue
+                visited[newi][newj]=1
+                queue.append((newi,newj))            
 
     def numIslands(self, grid):
         """
         :type grid: List[List[str]]
         :rtype: int
         """
-        count = 0
         rows = len(grid)
         cols = len(grid[0])
-        visited = [[0 for _ in range (cols)] for _ in range (rows)]
-
+        visited = [[0 for _ in range(cols)] for _ in range(rows)]
+        count = 0
         for r in range(rows):
-            for c in range(cols):
-                if grid[r][c]=="1" and visited[r][c]==0:
-                    count +=1
-                    self.dfs(r,c, rows, cols, visited, grid)
-    
+            for c in range (cols):
+                if grid[r][c]=='1' and visited[r][c]==0:
+                    count+=1
+                    self.bfs(r,c,grid,visited)
         return count
+    
