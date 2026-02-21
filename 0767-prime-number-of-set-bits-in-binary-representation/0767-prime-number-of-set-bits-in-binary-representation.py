70324fb0f0a90e820ab7d6abf0080d94f4274f28
Shvet Mehra <shvetmehra@gmail.com>
2026-02-21 08:49:32 +0530

    0767-PRIME-NUMBER-OF-SET-BITS-IN-BINARY-REPRESENTATION Stats: Time: 95 ms (90.36%), Space: 12.9 MB (18.78%) - LeetHub

--- 0767-prime-number-of-set-bits-in-binary-representation/0767-prime-number-of-set-bits-in-binary-representation.py
+++ 0767-prime-number-of-set-bits-in-binary-representation/0767-prime-number-of-set-bits-in-binary-representation.py
@@ -5,17 +5,14 @@ class Solution(object):
         :type right: int
         :rtype: int
         """
-        
+        # Optimized Version
+
+        primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31}
         res = 0
         for i in range (left, right+1):
-            count1 = 0
-            bin_str = '{:032b}'.format(i)
-            for j in bin_str:
-                if j=='1':
-                    count1 +=1
-            if count1 > 1 and all(count1 % x != 0 for x in range(2, int(count1**0.5) + 1)):
-
-                res+=1
-        return res
-
+            count1 = bin(i).count('1')
+            # Fatest built-in way in pythong to count!
+            if count1 in primes:
+                res +=1
+        return res 
         
