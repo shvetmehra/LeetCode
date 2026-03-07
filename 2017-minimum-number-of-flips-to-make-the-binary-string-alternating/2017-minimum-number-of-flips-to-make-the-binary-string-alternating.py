bc300b58fcfead0b7239e4502822927cd2c05674
Shvet Mehra <shvetmehra@gmail.com>
2026-03-07 12:02:56 +0530

    2017-MINIMUM-NUMBER-OF-FLIPS-TO-MAKE-THE-BINARY-STRING-ALTERNATING Stats: Time: 551 ms (16.94%), Space: 19.8 MB (94.35%) - LeetHub

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/2017-minimum-number-of-flips-to-make-the-binary-string-alternating.py
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/2017-minimum-number-of-flips-to-make-the-binary-string-alternating.py
@@ -7,25 +7,29 @@ class Solution:
         flip1 = 0
         flip2 = 0
         res = float('inf')
-        s1 = ""
-        s2 = ''
-        for x in range (2*n):
-            s1 += '0' if x%2 == 0 else '1'
-            s2 += '1' if x%2 == 0 else '0'
               
         while j <2*n:
-            if s[j%n]!= s1[j]:
-                flip1+=1
-            if s[j%n]!= s2[j]:
-                flip2+=1
+            expected_s1 = '0' if j % 2 == 0 else '1'
+            expected_s2 = '1' if j % 2 == 0 else '0'
             
-            if (j-i+1)>n:
-                if s[i%n]!=s1[i]:
-                    flip1 -=1
-                if s[i%n]!=s2[i]:
-                    flip2 -=1
-                i+=1
-            if (j-i+1)==n:
+            if s[j % n] != expected_s1:
+                flip1 += 1
+            if s[j % n] != expected_s2:
+                flip2 += 1
+            
+            if (j - i + 1) > n:
+                old_s1 = '0' if i % 2 == 0 else '1'
+                old_s2 = '1' if i % 2 == 0 else '0'
+                
+                if s[i % n] != old_s1:
+                    flip1 -= 1
+                if s[i % n] != old_s2:
+                    flip2 -= 1
+                i += 1
+            
+            if (j - i + 1) == n:
                 res = min(res, flip1, flip2)
-            j +=1
+                
+            j += 1
+            
         return res
