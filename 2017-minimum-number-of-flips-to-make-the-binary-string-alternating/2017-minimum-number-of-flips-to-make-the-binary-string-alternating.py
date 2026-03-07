6106f3a4e02e99311b6a5bd3a01065f62bb3bdfa
Shvet Mehra <shvetmehra@gmail.com>
2026-03-07 11:55:35 +0530

    2017-MINIMUM-NUMBER-OF-FLIPS-TO-MAKE-THE-BINARY-STRING-ALTERNATING Stats: Time: 650 ms (8.61%), Space: 20.9 MB (62.1%) - LeetHub

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/2017-minimum-number-of-flips-to-make-the-binary-string-alternating.py
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/2017-minimum-number-of-flips-to-make-the-binary-string-alternating.py
@@ -1,7 +1,7 @@
 class Solution:
     def minFlips(self, s: str) -> int:
         n = len(s)
-        s = (s+s)
+        
         i = 0
         j = 0
         flip1 = 0
@@ -9,20 +9,20 @@ class Solution:
         res = float('inf')
         s1 = ""
         s2 = ''
-        for x in range (len(s)):
+        for x in range (2*n):
             s1 += '0' if x%2 == 0 else '1'
             s2 += '1' if x%2 == 0 else '0'
               
         while j <2*n:
-            if s[j]!= s1[j]:
+            if s[j%n]!= s1[j]:
                 flip1+=1
-            if s[j]!= s2[j]:
+            if s[j%n]!= s2[j]:
                 flip2+=1
             
             if (j-i+1)>n:
-                if s[i]!=s1[i]:
+                if s[i%n]!=s1[i]:
                     flip1 -=1
-                if s[i]!=s2[i]:
+                if s[i%n]!=s2[i]:
                     flip2 -=1
                 i+=1
             if (j-i+1)==n:
