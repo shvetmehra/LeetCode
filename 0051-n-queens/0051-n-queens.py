98f11588be344f306c3642ee4ee5ab97d170b87f
Shvet Mehra <shvetmehra@gmail.com>
2026-03-22 18:24:08 +0530

    0051-N-QUEENS Stats: Time: 7 ms (96.11%), Space: 19.6 MB (83.75%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,44 +1,30 @@
-# BRUTE FORCE
 class Solution:
-    def isSafe(self, row, col, board, n):
-        r = row
-        c = col
-
-        while r>=0 and c>=0:
-            if board[r][c]=='Q':
-                return False
-            r-=1
-            c-=1
-        
-        r = row #We have to start from Fresh always
-        c = col
-        while c>=0:
-            if board[r][c]=='Q':
-                return False
-            c-=1
-
-        r = row
-        c = col
-        while r<n and c>=0:
-            if board[r][c]=='Q':
-                return False
-            r +=1
-            c -=1
-        return True
-
-    def solve(self, col, board, ans, n):
-        if col==n:
-            ans.append(list(board))
+    def solve(self, col, board, ans, leftrow, updiag, lowdiag, n):
+        if col == n:
+            ans.append(board[:])
             return
-        
-        for row in range (n):
-            if self.isSafe(row, col, board, n):
-                board[row]=board[row][:col] + 'Q' + board[row][col +1:]
-                self.solve (col+1, board, ans, n)
-                board[row]=board[row][:col] + '.' + board[row][col +1:]
+        for row in range(n):
+            if (
+                leftrow[row] == 0
+                and updiag[n-1+col-row]==0
+                and lowdiag[row+col]==0
+            ):
+                board[row]=board[row][:col]+'Q'+board[row][col+1:]
+                leftrow[row]=1
+                updiag[n-1+col-row]=1
+                lowdiag[col+row]=1
+
+                self.solve(col+1,board,ans, leftrow,updiag,lowdiag,n)
+                board[row]=board[row][:col]+'.'+board[row][col+1:]
+                leftrow[row]=0
+                updiag[n-1+col-row]=0
+                lowdiag[col+row]=0
 
     def solveNQueens(self, n: int) -> List[List[str]]:
         ans = []
-        board = ['.' * n for _ in range (n)]
-        self.solve(0, board, ans, n)
+        board = ['.' *n for _ in range(n)]
+        leftrow = [0]*n
+        updiag = [0]*(2*n-1)
+        lowdiag = [0]*(2*n-1)
+        self.solve(0, board, ans, leftrow, updiag, lowdiag, n )
         return ans
