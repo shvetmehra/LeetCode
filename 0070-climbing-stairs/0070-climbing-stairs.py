749fbdb3e7f76470438692c592ff9ec38104a08d
Shvet Mehra <shvetmehra@gmail.com>
2026-03-25 16:21:32 +0530

    0070-CLIMBING-STAIRS Stats: Time: 0 ms (100%), Space: 19.2 MB (93.47%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,15 +1,15 @@
-#recursion 
+# tabulation bottoms up
 class Solution:
-    def solve(self, n, dp):
-        if n <=2:
-            return n
-
-        if dp[n]!=-1:
-            return dp[n]
-        
-        dp[n] = self.solve(n-1,dp) + self.solve(n-2,dp)
-        return dp[n]
-
     def climbStairs(self, n: int) -> int:
-        dp = [-1] * (n+1)
-        return self.solve(n,dp)
+        if n<=2:
+            return n
+        dp = [-1]*(n+1)
+        dp[1] = 1
+        dp[2] = 2
+        
+
+        for i in range (3, n+1):
+            dp[i] = (dp[i-1] ) + (dp[i-2])
+
+
+        return dp[n]
