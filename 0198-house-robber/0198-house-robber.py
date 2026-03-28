e32633458e0d4d645d2d34b887966a7149b4116f
Shvet Mehra <shvetmehra@gmail.com>
2026-03-28 12:25:16 +0530

    0198-HOUSE-ROBBER Stats: Time: 0 ms (100%), Space: 19.3 MB (76.53%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,21 +1,18 @@
-# Tabulation - Bottoms Up
+# Tabulation Space Optimaztion
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp = [-1]*n
-        dp[0] = nums[0]
-        if n == 0:
-            return 0
-        if n == 1:
-            return nums[0]        
+        n = len (nums)
+        prev = nums[0]
+        prev2 = 0
 
         for index in range (1, n):
             if index>1:
-                pick = nums[index] + dp[index-2]
+                pick = nums[index]+prev2
             else:
-                pick = nums[index] +0
+                pick = nums[index]
+            notpick = prev
         
-            notpick = 0+dp[index-1]
-
-            dp[index] = max(pick, notpick)
-        return dp[n-1]
+            curr = max (pick, notpick)
+            prev2 = prev
+            prev = curr
+        return prev
