04463898fdf25337ee1b95655e1acb86663b8b48
Shvet Mehra <shvetmehra@gmail.com>
2026-03-30 08:53:57 +0530

    2978-CHECK-IF-STRINGS-CAN-BE-MADE-EQUAL-WITH-OPERATIONS-II Stats: Time: 131 ms (52.24%), Space: 20.2 MB (68.66%) - LeetHub

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/2978-check-if-strings-can-be-made-equal-with-operations-ii.py
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/2978-check-if-strings-can-be-made-equal-with-operations-ii.py
@@ -1,10 +1,14 @@
 class Solution:
     def checkStrings(self, s1: str, s2: str) -> bool:
         n = len(s1)
-        e1 = sorted(s1[i] for i in range (0, n, 2))
-        e2 = sorted(s2[i] for i in range (0, n, 2))
-        o1 = sorted(s1[i] for i in range (1, n, 2))
-        o2 = sorted(s2[i] for i in range (1, n, 2))
+        e1, o1 = defaultdict(int), defaultdict(int)
+        e2, o2 = defaultdict(int), defaultdict(int)
 
-        return e1==e2 and o1==o2
-        
+        for i in range (n):
+            if i %2 == 0:
+                e1[s1[i]] +=1
+                e2[s2[i]] +=1
+            else:
+                o1[s1[i]] +=1
+                o2[s2[i]] +=1
+        return e1 == e2 and o1 == o2
