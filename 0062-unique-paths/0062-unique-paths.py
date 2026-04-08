1db941d6b345e4969e9303a4c0257d9923fe6962
Shvet Mehra <shvetmehra@gmail.com>
2026-04-08 18:03:34 +0530

    0062-UNIQUE-PATHS Stats: Time: 0 ms (100%), Space: 19.3 MB (82.22%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,16 +1,20 @@
 class Solution:
-    def solve(self, i, j, dp):
-        if i == 0 and j== 0:
-            return 1
-        if i<0 or j<0:
-            return 0
-        if dp[i][j]!=-1:
-            return dp[i][j]
-        
-        up = self.solve(i-1, j, dp)
-        left = self.solve(i, j-1, dp)
-        dp[i][j]  = up + left
-        return dp[i][j]
     def uniquePaths(self, m: int, n: int) -> int:
         dp = [[-1 for _ in range (n)] for _ in range (m)]
-        return self.solve(m-1,n-1, dp)
+        dp[0][0]=1
+
+        for i in range (0,m):
+            for j in range(0,n):
+                if i==0 and j==0:
+                    continue
+                if i>0:
+                    up = dp[i-1][j]
+                else:
+                    up = 0
+                if j ==0:
+                    left = 0
+                else:
+                    left = dp[i][j-1]
+                
+                dp[i][j]=up+left
+        return dp[m-1][n-1]
