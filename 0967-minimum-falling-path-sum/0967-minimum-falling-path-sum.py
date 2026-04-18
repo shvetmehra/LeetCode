4ec71d50ff4f6a124a6e1f4123352c1eb9f7d404
Shvet Mehra <shvetmehra@gmail.com>
2026-04-18 18:07:58 +0530

    0967-MINIMUM-FALLING-PATH-SUM Stats: Time: 12 ms (79.93%), Space: 19.9 MB (98.07%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.py
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.py
@@ -2,23 +2,25 @@ class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         rows = len(matrix)
         cols = len(matrix[0])
-        dp = [[None for _ in range(cols)] for _ in range (rows)]
-        for j in range(cols):
-            dp[0][j] = matrix[0][j]
+        prev = [None for _ in range (cols)]
+        for j in range (cols):
+            prev[j] = matrix[0][j]
         for i in range(1, rows):
+            curr = [None]*cols
             for j in range(cols):
                 if j==0:
                     left = float('inf')
                 else:
-                    left = matrix[i][j]+dp[i-1][j-1]
+                    left = prev[j-1]
                 if j==cols-1:
                     right =  float('inf')
                 else:
-                    right = matrix[i][j] + dp[i-1][j+1]
-                up = matrix[i][j]+dp[i-1][j]
-                dp[i][j]=min(up, left, right)
+                    right = prev[j+1]
+                up = prev[j]
+                curr[j] = matrix[i][j]+min(up, left, right)
+            prev = curr
         ans = float('inf')
         for j in range(cols):
-            ans = min(ans, dp[rows-1][j])
-        return ans 
+            ans = min(ans, prev[j])
+        return ans
         
