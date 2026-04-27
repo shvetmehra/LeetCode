d6ff68dd1da1311db861874406d67e51fd03274a
Shvet Mehra <shvetmehra@gmail.com>
2026-04-27 19:44:57 +0530

    1507-CHECK-IF-THERE-IS-A-VALID-PATH-IN-A-GRID Stats: Time: 197 ms (58.58%), Space: 35.6 MB (61.57%) - LeetHub

--- 1507-check-if-there-is-a-valid-path-in-a-grid/1507-check-if-there-is-a-valid-path-in-a-grid.py
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/1507-check-if-there-is-a-valid-path-in-a-grid.py
@@ -1,26 +1,37 @@
+from collections import deque
+from typing import List
 class Solution:
-    def dfs(self, r,c,grid,visited, rows, cols, directions):
-        if r == rows-1 and c==cols-1:
-            return True
-        visited.add((r,c))
-
-        for dr, dc in directions[grid[r][c]]:
-            newRow, newCol = r+dr, c+dc
-            if 0 <= newRow < rows and 0 <= newCol < cols and (newRow, newCol) not in visited:
-                if (-dr,-dc) in directions[grid[newRow][newCol]]:
-                    if self.dfs(newRow, newCol, grid, visited,rows, cols, directions  ):
-                        return True
-        return False
     def hasValidPath(self, grid: List[List[int]]) -> bool:
         rows = len(grid)
         cols = len(grid[0])
         directions = {
-            1: [(0, -1), (0, 1)],
-            2: [(-1, 0), (1, 0)],
-            3: [(0, -1), (1, 0)],
-            4: [(0, 1), (1, 0)],
-            5: [(0, -1), (-1, 0)],
-            6: [(0, 1), (-1, 0)]
-        }
-        visited = set()
-        return self.dfs(0,0,grid, visited, rows, cols, directions)
+                    1: [(0, -1), (0, 1)],
+                    2: [(-1, 0), (1, 0)],
+                    3: [(0, -1), (1, 0)],
+                    4: [(0, 1), (1, 0)],
+                    5: [(0, -1), (-1, 0)],
+                    6: [(0, 1), (-1, 0)]
+                }
+        queue = deque([(0,0)])
+        visited = set([(0,0)])
+        # for r in range (rows):
+        #     for c in range (cols):
+        #         if r==0 or c==0 or r==rows-1 or c==cols-1:
+        #             queue.append((r,c))
+        #             visited[r][c]=1
+        while len(queue)!=0:
+            i,j=queue.popleft()
+            if (i,j)==(rows-1, cols-1):
+                return True
+            for dr,dc in directions[grid[i][j]]:
+                newi,newj=i+dr, j+dc
+                if 0<=newi<rows and 0<= newj<cols:
+                    if( newi, newj) in visited:
+
+                        continue
+                
+                    if (-dr,-dc) in directions[grid[newi][newj]]:
+                        visited.add((newi, newj))
+                        queue.append((newi, newj))
+        return False
+   
