be67372dbab5f6669f6e95a64b04ff787976d9c4
Shvet Mehra <shvetmehra@gmail.com>
2026-04-30 19:43:15 +0530

    3986-MAXIMUM-PATH-SCORE-IN-A-GRID Stats: Time: 13092 ms (19.69%), Space: 37.9 MB (45.6%) - LeetHub

--- 3986-maximum-path-score-in-a-grid/3986-maximum-path-score-in-a-grid.py
+++ 3986-maximum-path-score-in-a-grid/3986-maximum-path-score-in-a-grid.py
@@ -1,30 +1,32 @@
-# Memoization  
+# Tabulation   
 
 class Solution:
-    def solve(self, i, j, k, grid, dp):
-        if k<0:
-            return float('-inf')
-        if i == 0 and j == 0:
-            cost = 1 if grid[0][0] in [1,2] else 0
-            if k>=cost:
-                return grid[0][0]
-            return float('-inf')
-            
-        if i<0 or j<0:
-            return float('-inf')
-        curr_score = grid[i][j]
-        curr_cost = 1 if grid[i][j] in [1,2] else 0
-        if dp[i][j][k]!=-1:
-            return dp[i][j][k]
-        up = self.solve(i-1, j,k-curr_cost, grid, dp)
-        left= self.solve(i, j-1,k-curr_cost, grid, dp)
-        res = curr_score + max(left, up)
-        dp[i][j][k]=res
-        return res
     def maxPathScore(self, grid: List[List[int]], k: int) -> int:
         m = len(grid)
         n = len(grid[0])
-        dp = [[[-1 for _ in range (k+1)] for _ in range (n)] for _ in range (m)]
-        
-        result= self.solve(m-1,n-1,k, grid, dp)
-        return int(result) if result>float('-inf') else -1
+        dp = [[[-float('inf')] * (k + 1) for _ in range(n)] for _ in range(m)]
+        val = grid[0][0]
+        start = grid[0][0]
+        cost = 1 if grid[0][0] in [1,2] else 0
+        if cost <=k:
+            dp[0][0][cost]= val
+        for i in range (m):
+            for j in range(n):
+                for c in range (k+1):
+                    if dp[i][j][c]==-float('inf'):
+                        continue
+                    if j + 1 < n:
+                        val = grid[i][j+1]
+                        cost = 1 if val in [1, 2] else 0
+                        if c + cost <= k:
+                            dp[i][j+1][c + cost] = max(dp[i][j+1][c + cost], dp[i][j][c] + val)
+                            
+                    # Try moving Down
+                    if i + 1 < m:
+                        val = grid[i+1][j]
+                        cost = 1 if val in [1, 2] else 0
+                        if c + cost <= k:
+                            dp[i+1][j][c + cost] = max(dp[i+1][j][c + cost], dp[i][j][c] + val)
+                                
+        ans = max(dp[m-1][n-1])
+        return int(ans) if ans > -float('inf') else -1
