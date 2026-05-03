6f4e695c83cba1731ab56e11542d2088c4506409
Shvet Mehra <shvetmehra@gmail.com>
2026-05-03 09:27:42 +0530

    0812-ROTATE-STRING Stats: Time: 0 ms (100%), Space: 19.3 MB (51.56%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.py
+++ 0812-rotate-string/0812-rotate-string.py
@@ -3,9 +3,5 @@ class Solution:
         n = len(s)
         if n != len(goal):
             return False
-        for i in range (n):
-            if s[i:]+s[:i] == goal:
-                return True
-        
-        return False
+        return goal in s + s
         
