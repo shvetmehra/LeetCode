2604c311e9be6b03117f540083aa115241ac5fb7
Shvet Mehra <shvetmehra@gmail.com>
2026-05-10 12:25:39 +0530

    2855-MAXIMUM-NUMBER-OF-JUMPS-TO-REACH-THE-LAST-INDEX Stats: Time: 851 ms (5.23%), Space: 19.4 MB (64.71%) - LeetHub

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/2855-maximum-number-of-jumps-to-reach-the-last-index.py
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/2855-maximum-number-of-jumps-to-reach-the-last-index.py
@@ -1,20 +1,15 @@
-# Memoization 
+# Tabulation 
 class Solution:
-    def solve(self, i, nums, target, n, dp):
-        if i == n-1:
-            return 0
-        if dp[i]!=-1:
-            return dp[i]
-        max_jump = float('-inf')
-        for j in range(i+1, n):
-            if abs(nums[j]-nums[i])<=target:
-                res = self.solve(j, nums, target, n, dp)
-                if res != float('-inf'):
-                    max_jump = max(max_jump, res+1)
-        dp[i]=max_jump
-        return dp[i]
     def maximumJumps(self, nums: List[int], target: int) -> int:
         n = len(nums)
-        dp = [-1]* n
-        ans = self.solve(0, nums, target, n, dp)
-        return ans if ans> float('-inf') else -1
+        dp = [float('-inf')]* n
+        dp [0] = 0
+
+        for i in range (1, n):
+            for j in range (i):
+                if dp[j]!= float('-inf') and abs(nums[i] - nums[j])<= target:
+                    dp[i] = max(dp[i], dp[j]+1)
+        if dp[n-1] == float('-inf'):
+            return -1
+        else:
+            return int(dp[n-1])
