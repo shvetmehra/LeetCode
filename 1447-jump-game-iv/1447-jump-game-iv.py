f5672c7e3437f7d81b870ca1ea8eaa169e2a93aa
Shvet Mehra <shvetmehra@gmail.com>
2026-05-18 11:38:21 +0530

    1447-JUMP-GAME-IV Stats: Time: 184 ms (55.41%), Space: 35.2 MB (34.04%) - LeetHub

--- 1447-jump-game-iv/1447-jump-game-iv.py
+++ 1447-jump-game-iv/1447-jump-game-iv.py
@@ -1,25 +1,26 @@
+from collections import defaultdict, deque
 class Solution:
     def minJumps(self, arr: List[int]) -> int:
         hashmap = defaultdict(list)
         for index, val in enumerate(arr):
             hashmap[val].append(index)
-        queue = []
+        queue = deque([0])
         steps = 0
         visited = set()
-
-        queue.append(0)
+        
         visited.add(0)
         while queue:
             queue_len = len(queue)
             for  _ in range (queue_len):
-                index = queue.pop(0)
+                index = queue.popleft()
                 if index == len(arr)-1:
                     return steps
-                for adj_index in hashmap.get(arr[index]):
+                for adj_index in hashmap.get(arr[index], []):
                     if adj_index not in visited:
                         queue.append(adj_index)
                         visited.add(adj_index)
                 hashmap[arr[index]]=[]
+                
                 if (index-1 >-1) and (index -1) not in visited:
                         queue.append(index-1)
                         visited.add(index-1)
