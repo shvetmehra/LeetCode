d010f334d72365c9cbdfe9ebb97f97f488098218
Shvet Mehra <shvetmehra@gmail.com>
2026-05-22 11:47:16 +0530

    0189-ROTATE-ARRAY Stats: Time: 0 ms (100%), Space: 26.4 MB (70.89%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -5,10 +5,4 @@ class Solution:
         """
         n = len(nums)
         k = k%n
-        rotated=[0]*n
-        for i in range (n):
-            rotated[(i+k)%n] = nums[i]
-        for i in range (n):
-            nums[i] = rotated[i]
-
-        
+        nums[:]=nums[-k:]+nums[:-k]
