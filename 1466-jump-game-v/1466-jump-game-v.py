471fdd56908e1ab3e2e8539b10ca1d51269a7090
Shvet Mehra <shvetmehra@gmail.com>
2026-05-24 10:19:13 +0530

    1466-JUMP-GAME-V Stats: Time: 176 ms (80.86%), Space: 19.5 MB (82.86%) - LeetHub

--- 1466-jump-game-v/1466-jump-game-v.py
+++ 1466-jump-game-v/1466-jump-game-v.py
@@ -1,31 +1,20 @@
-# emoization 
+# Tabulation
 class Solution:
-    def solve(self, i, arr, d, n, dp):
-        maxjump = 1
-        if dp[i]!=-1:
-            return dp[i]
-        for x in range (1, d+1):
-            j = i+x
-            if j>=n:
-                break
-            if arr[j]>=arr[i]:
-                break
-            maxjump = max(maxjump, 1+self.solve(j, arr, d, n, dp))
-
-        for x in range (1, d+1):
-            j = i-x
-            if j<0:
-                break
-            if arr[j]>=arr[i]:
-                break
-            maxjump = max(maxjump, 1+self.solve(j, arr, d, n, dp))
-        dp[i] = maxjump
-        return dp[i]
-
     def maxJumps(self, arr: List[int], d: int) -> int:
         n = len(arr)
-        dp = [-1]*n
-        ans = 0
-        for i in range (n):
-            ans = max(ans, self.solve(i, arr, d, n, dp))
-        return ans
+        dp = [1]*n
+        sorted_arr = sorted((val, idx)for idx , val in enumerate (arr))
+
+        for val, i in sorted_arr:
+            for x in range (1, d+1):
+                j = i+x
+                if j>=n or arr[j]>=arr[i]:
+                    break
+                dp[i]= max(dp[i], 1+dp[j])
+
+            for x in range (1, d+1):
+                j = i-x
+                if j <0 or arr[j]>=arr[i]:
+                    break
+                dp [i] = max(dp[i], 1+dp[j])
+        return max(dp)
